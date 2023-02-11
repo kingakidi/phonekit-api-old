0c042b0e58a3f27de7d1a9fc3fbd1200fa5ebfe6
Aka'aba Musa Akidi <onlinemraam@gmail.com>
2023-02-11 11:43:00 +0100

    login completed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,14 +6,16 @@ exports.login = async (req, res) => {
   try {
     // check if the email exist
     const { email, password } = req.body;
-    const isUser = await db.query("SELECT * FROM profile WHERE email = ?", [
-      email,
-    ]);
+    const isUser = await db.query(
+      "SELECT * FROM profile WHERE email = ? LIMIT 1",
+      [email]
+    );
 
     if (isUser[0].length > 0) {
       //verify user password, and generate jwt token
       const user = isUser[0];
-      const db_password = user.password;
+
+      const db_password = user[0].password;
 
       bcrypt.compare(password, db_password, function (err, result) {
         if (!result)
@@ -29,17 +31,14 @@ exports.login = async (req, res) => {
         };
 
         const token = jwt.sign(payload, process.env.SECRET);
+        delete user[0].password;
 
         return res.status(200).send({
           status: "success",
           message: "login successfully",
 
           data: {
-            user: {
-              fullname: user.fullname,
-              permissionId: user.permissionId,
-              userId: user.id,
-            },
+            user: user[0],
             token,
           },
         });
@@ -52,6 +51,7 @@ exports.login = async (req, res) => {
     }
   } catch (error) {
     console.log(error.message);
+
     return res.status(500).json({
       status: 500,
       message: "Something went wrong",
