6152467119aced88566bacb11c1aad872802f5bd
Aka'aba Musa Akidi <onlinemraam@gmail.com>
2023-02-19 21:48:46 +0100

    added

--- middlewares/validation/auth.validation.js
+++ middlewares/validation/auth.validation.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 
 exports.auth_login = async (req, res, next) => {
-  console.log(req.body);
   // check if the inputs are complates,
   const schema = Joi.object({
     email: Joi.string().email().required(),
@@ -11,7 +10,6 @@ exports.auth_login = async (req, res, next) => {
   const { error } = schema.validate(req.body);
 
   if (error) {
-    console.log();
     return res.status(400).json({
       sucess: false,
       message: "validation failed",
